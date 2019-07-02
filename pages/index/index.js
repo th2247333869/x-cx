00aa08b3856a85b62a79f3c607e96b2fb91c0f44
2247333869 <2247333869@qq.com>
2019-07-02 22:03:17 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,115 @@
+//index.js
+//获取应用实例
+var utils = require('../../utils/util.js')
+
+const app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {    
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e.detail.userInfo)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  doLogin:function(e){
+   let that = this;
+    wx.login({
+      success(res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: utils.getUrl() +'wxlogin',
+            data: {
+              code: res.code,//获取openid的话 需要向后台传递code,利用code请求api获取openid
+              headurl: app.globalData.userInfo.avatarUrl,//这些是用户的基本信息
+              nickname: app.globalData.userInfo.nickName,//获取昵称
+              sex: app.globalData.userInfo.gender,//获取性别
+              country: app.globalData.userInfo.country,//获取国家
+              province: app.globalData.userInfo.province,//获取省份
+              city: app.globalData.userInfo.city//获取城市
+            },
+            success: function (res) {
+              console.log(res.data.message);
+              that.jumpPage();
+            }
+          })   
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+
+  getUUid:function(){
+    console.log(utils.getUrl()+this.userInfo)
+    wx.request({
+      url:"test",
+      method: 'POST',
+      header: { 'content-type': 'application/x-www-form-urlencoded' ,
+                'Authorization': "1234"},
+      success: function (res) {
+        console.log(res.data + "你是从后台来的ma?");
+        
+      },
+      fail: function (res) {
+        console.log('submit fail');
+      },
+      complete: function (res) {
+        console.log('submit complete');
+      },
+    })
+  },
+
+  jumpPage:function () {
+    wx.navigateTo({
+      url: '/pages/index/module/chat'
+    })
+  }
+})
+/**
+ * 跳转页面
+ */
+
+
---
+++ pages/index/module/chat.js
@@ -0,0 +1,173 @@
+// pages/index/module/chat.js
+
+var utils = require('../../../utils/util.js')
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    firstFocuse:1,
+    isChange:{
+      isColect1:true,
+      isColect2: false,
+      isColect3: false,
+      isColect4: false,
+    },
+    currentData:0,  
+    userInfo: {},
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  /**
+   * 获取焦点跳转页面
+   */
+  searchPage: utils.throttle(function (e) {
+    wx.navigateTo({
+      url: './search',
+    })
+  }, 3000),
+
+  catchTouchMove: function (res) {
+    return false
+  },
+
+  goPersonal:function(){
+    wx.navigateTo({
+      url: './../pages/personal',
+    })
+  },
+
+  panelActive:function(arg){
+    var that = this;
+    if (that.data.currentData != arg.currentTarget.dataset.gid) {
+      that.setData({
+        currentData: arg.currentTarget.dataset.gid
+      })
+    }
+    if (arg.currentTarget.dataset.gid == 0){
+      that.setData({
+        isChange: {
+          isColect1:true,
+          isColect2:false,
+          isColect3: false,
+          isColect4: false,
+        },
+      });
+    } else if (arg.currentTarget.dataset.gid == 1) {
+      that.setData({
+        isChange: {
+          isColect1: false,
+          isColect2: true,
+          isColect3: false,
+          isColect4: false,
+        },
+      });
+    } else if (arg.currentTarget.dataset.gid  == 2) {
+      that.setData({
+        isChange: {
+          isColect1: false,
+          isColect2: false,
+          isColect3: true,
+          isColect4: false,
+        },
+      });
+    } else if (arg.currentTarget.dataset.gid  == 3) {
+      that.setData({
+        isChange: {
+          isColect1: false,
+          isColect2: false,
+          isColect3: false,
+          isColect4: true,
+        },
+      });
+    }
+  },
+
+  bindchange: function (e) {
+    const that = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
